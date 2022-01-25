99616381ef02687c9d540642f31ea9c6dff4e671
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-01-24 19:49:21 -0500

    added get all, get 1 and create user routes/controllers

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,5 +1,36 @@
 const { User, Game, Comment } = require('../models');
+const { populate } = require('../models/User');
+
+const userController = {
+    // Post new user
+    addUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'comments',
+                select: '-__V'
+            })
+            .select('-__V')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+    // get single user
+    getUserById({params}, res) {
+        User.findOne({ _id: params.id })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    }
+};
 
 
 
-module.exports = userControllers;
+module.exports = userController;
