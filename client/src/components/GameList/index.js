b71bc2b2862c0b0e993a3b7a4cab28573c82a9ee
kelbach <87092340+Kelbach@users.noreply.github.com>
2022-01-28 16:52:30 -0500

    GameList contains getAllGames api call

--- client/src/components/GameList/index.js
+++ client/src/components/GameList/index.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../utils/api';
 
 const GameList = () => {
-    //Test data
-    const [games] = useState([
-        { _id: "5432", name: 'Skyrim', description: 'Open world medieval role playing game' },
-        { _id: "4567", name: 'Mario Kart', description: 'Action packed racing game' },
-        { _id: "9876", name: 'Soul Calliber', description: 'Button mashing fighter' },
-        { _id: "1234", name: 'Animal Crossing', description: 'Cute farm animals' }
-    ]);
+    // //Test data
+    // const [games] = useState([
+    //     { _id: "5432", name: 'Skyrim', description: 'Open world medieval role playing game' },
+    //     { _id: "4567", name: 'Mario Kart', description: 'Action packed racing game' },
+    //     { _id: "9876", name: 'Soul Calliber', description: 'Button mashing fighter' },
+    //     { _id: "1234", name: 'Animal Crossing', description: 'Cute farm animals' }
+    // ]);
+
+    const [gameData, setGameData] = useState([]);
+    useEffect(()=>{
+        api.getAllGames().then(res=>{
+            
+            if (res.status < 200 || res.status > 299 ) {
+                throw new Error('something went wrong!');
+            }
+            setGameData(res.data)
+        })
+        
+    },[]);
 
     return (
         <div className='gameList'>
@@ -16,9 +29,9 @@ const GameList = () => {
                 Top Games of 2021:
             </h3>
             <ol>
-                {games.map((game) => (
-                    <li key={game.name}>
-                        <Link to={'/game/' + game._id}>{game.name}</Link>
+                {gameData.map((game) => (
+                    <li key={game.title}>
+                        <Link to={'/game/' + game._id}>{game.title}</Link>
                     </li>
                 ))}
             </ol>
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,41 @@
-import React from 'react';
+// import React, { useState, useEffect } from 'react';
+// import Auth from '../../utils/auth'
+// import api from '../../utils/api';
+
+
 
 function Dashboard(){
+    // // starter function for calling favedGames
+    // // waiting for route
+    // const [faveGameData, setFaveGameData] = useState({});
+    // const faveGameDataLength = Object.keys(faveGameData).length;
+
+    // useEffect(() => {
+    //     const getFaveGameData = async () => {
+    //         try {
+    //             const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    //             if (!token) {
+    //             return false;
+    //             }
+
+    //             const response = await api.getUser(token);
+
+    //             if (!response.ok) {
+    //             throw new Error('something went wrong!');
+    //             }
+
+    //             const user = await response.json();
+    //             setFaveGameData(user);
+    //         } catch (err) {
+    //             console.error(err);
+    //         }
+    //         };
+    //         getFaveGameData()
+    //     }, [faveGameDataLength]
+    // );
+
+
     return(
         <div>Dashboard</div>
     )
