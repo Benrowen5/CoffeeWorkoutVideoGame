3e1827a2ea31a7b0a6a920f3eaacfbe5bf37e2c2
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-02-02 00:39:34 -0500

    modified routes/index for deployment

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,8 @@
 const router = require('express').Router();
-const htmlRoutes = require('./html/html-routes');
 // import all API routes from /api/index
 const apiRoutes = require('./api');
 
 // add /api prefix to all of the api routes imported from api directory
 router.use('/api', apiRoutes);
-router.use('/', htmlRoutes);
-
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 module.exports = router;
