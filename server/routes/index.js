501bd0017d0fabb43928f99cc4c7bc320a952932
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-02-02 00:37:19 -0500

    modified routes/index for deployment

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
-  res.status(404).send('<h1>😝 404 Error!</h1>');
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 module.exports = router;
