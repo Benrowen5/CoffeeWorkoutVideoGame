d5cb7b61b6b9c0831292e45a92ea9ea52274804e
Ikenna Nwauwa <icknwauwa15@gmail.com>
2022-02-01 22:28:46 -0500

    changed home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import About from '../components/About';
 import GameList from '../components/GameList';
-<<<<<<< Updated upstream
 import TopGames from '../components/TopGames';
-=======
 import styles from '../pages/Dashboard.module.css'
->>>>>>> Stashed changes
 
 const Home = () => {
     return (
