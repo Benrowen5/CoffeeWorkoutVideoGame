c9d47f204a4a0ecc6483ace4e0afc8a4b1696e1e
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-02-01 21:57:33 -0500

    added topgames component to homepage.

--- client/src/components/TopGames/index.js
+++ client/src/components/TopGames/index.js
@@ -12,9 +12,9 @@ const TopGames = () => {
                     throw new Error('something went wrong!');
                 }
                 const data = res.data;
+                // sort game results by favoriteCount value
                 data.sort((a, b) => (a.FavoriteCount < b.FavoriteCount) ? 1: -1);
-                console.log(data);
-        
+                // create new array for top 3 favorited games
                 const topThreeArray = data.slice(0, 3);
                 setTopFavorites(topThreeArray)
             });
@@ -22,7 +22,7 @@ const TopGames = () => {
 
     return (
         <div className="favoriteList">
-            <h3>Top Games:</h3>
+            <h3>Featured Games:</h3>
             <ol>
                 {topFavorites.map((favorite) => (
                     <li key={favorite.title}>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import About from '../components/About';
+import GameList from '../components/GameList';
 import TopGames from '../components/TopGames';
 
 const Home = () => {
     return (
         <div>
             <About/>
+            <TopGames />
             <GameList/>
         </div>
     );
