12857bc00aabb82016f36e805865bdb952e599d5
Ikenna Nwauwa <icknwauwa15@gmail.com>
2022-02-02 20:51:32 -0500

    Fixed Dashboard

--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Game = ({id, img, alt, title, desc}) => {
     return (
-        <div className={styles.game}>
+        <Link className={styles.game} to={'/game/' + id}>
             <img className={styles.gameCover} src={img} alt={alt}/>
             <Link className={styles.gameTitle} to={'/game/' + id}>{title}</Link>
-            <p className={styles.gameDesc}>{desc}</p>
-        </div>
+            {/* <p className={styles.gameDesc}>{desc}</p> */}
+        </Link>
     );
 }
 
